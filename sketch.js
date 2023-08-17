b977cff546bda3346959e46ebd43e66fe4d1d384
HypeDragon88 <142500420+HypeDragon88@users.noreply.github.com>
2023-08-17 16:00:42 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var colour1 
+var colour2
+var colour3
+var state
+
+
+
+function setup() {
+  createCanvas(1500,1000);
+  colour1 = 0
+  colour2 = 0
+  colour3 = 20
+  state = "forwards"
+}
+
+
+
+function draw() 
+{
+  background(30+ World.mouseX,30 + World.mouseY,150);
+
+}
+
+
